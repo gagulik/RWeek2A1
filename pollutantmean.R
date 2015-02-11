bebfbc51af3a970abe85386690ae4897b5027f62
Sardar F. Azari <me@sardar.net>
2015-02-10 23:39:05 -0500

    First cut of pollutantmean.R

--- pollutantmean.R
+++ pollutantmean.R
@@ -11,4 +11,34 @@ pollutantmean <- function(directory, pollutant, id = 1:332) {
   
   ## Return the mean of the pollutant across all monitors list
   ## in the 'id' vector (ignoring NA values)
+        
+        fExt  <- "csv"     # Data File extension
+        fNames <- sprintf("%03d",id) # add prepending zeroes to id to form file names
+        
+        # Construct file path in a platform-indpendent manner
+        FullPathes <- file.path(directory, paste(fNames, fExt, sep="."), fsep = .Platform$file.sep)
+        
+        # Create vector of mean values for each monitor
+        MeanS <- numeric(length(FullPathes))
+
+        # Enumerate through all files
+        for (i in seq_along(FullPathes)) {
+                DataFile <- FullPathes[i]
+                #print(DataFile)
+                Data <- read.csv(DataFile)
+                
+                # Filter-out NA values
+                Poll <- Data[[pollutant]] # get pollutant as vector
+                Poll2 <- Poll[!is.na(Poll)] # remove NA values
+                #CleanData <- Data[!is.na(Data[pollutant]),]
+                
+                # Get vector from Data Frame column and calculate it's mean
+                MeanS[i] <- mean(Poll2)
+                #MeanS[i] <- mean( CleanData[[pollutant]] )
+                #print(MeanS[i])
+        }
+        
+        # Return value -  mean of means
+        FinalMean <- mean(MeanS)
+        FinalMean
 }
